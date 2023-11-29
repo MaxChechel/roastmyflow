f50c09d2f3660483725a57493a925e1be02262d6
Max Chechel <maksymchechel@yahoo.com>
2023-11-29 11:08:54 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
+
+export default defineConfig({
+    plugins: [eslintPlugin({ cache: false })],
+    server: {
+        port: 3000,
+    },
+    build: {
+        minify: true,
+        manifest: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: '[name].js',
+            },
+        },
+    },
+});
